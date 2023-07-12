04c2136440feb2e6d88cb9a48116a1ec56568505
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-07-12 09:40:42 -0300

    Criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 AOS.init();
+
+const dataDoEvento = new Date("Dec 10, 2023 00:00:00");
+const timestampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+  const agora = new Date ();
+  const timestampAtual = agora.getTime();
+
+  const distanciaAteOEvento =  timestampDoEvento - timestampAtual  ;
+
+  const diaEmMs = 1000 * 60 * 60 * 24;
+  const horasEmMs = 1000 * 60 * 60;
+  const minutosEmMs = 1000 * 60;
+
+  const diasAteOEvent = Math.floor(distanciaAteOEvento / diaEmMs)
+  const horasAteOEvento= Math.floor((diasAteOEvent % diaEmMs) / horasEmMs );
+  const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+  const segundosAteOEvento =Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+  
+  document.getElementById('contador').innerHTML = `${diasAteOEvent}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+ if(distanciaAteOEvento < 0 ){
+  clearInterval(contaAsHoras);
+  document.getElementById('contador').innerHTML ="Evento expirado"
+ }
+
+
+}, 1000)
